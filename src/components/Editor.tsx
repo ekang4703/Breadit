89cfe09a9778de479b1219d3bf9306a2f5ac2050
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-09-28 21:10:57 -0700

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -24,6 +24,46 @@ import { useCustomToasts } from '@/hooks/use-custom-toasts'
 
 type FormData = z.infer<typeof PostValidator>
 
+
+
+interface CreateCommentProps {
+  postId: string
+  replyToId?: string
+}
+
+const CreateComment: FC<CreateCommentProps> = ({ postId, replyToId }) => {
+  const [input, setInput] = useState<string>('')
+  const router = useRouter()
+  const { loginToast } = useCustomToasts()
+
+  const { mutate: comment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload2: CommentRequest = { postId, text, replyToId }
+
+      const { data } = await axios.patch(
+        `/api/subreddit/post/comment/`,
+        payload2
+      )
+      return data
+    },
+
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast()
+        }
+      }
+
+      return toast({
+        title: 'Something went wrong.',
+        description: "Comment wasn't created successfully. Please try again.",
+        variant: 'destructive',
+      })
+    },
+  })
+
+
+
 interface EditorProps {
   subredditId: string
 }

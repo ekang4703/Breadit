77c6cb7f814c84fe203deec2fbfa4bd275ae9ddd
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-09-28 21:15:24 -0700

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -31,7 +31,40 @@ interface CreateCommentProps {
   replyToId?: string
 }
 
+const CreateComment: FC<CreateCommentProps> = ({ postId, replyToId }) => {
+  const [input, setInput] = useState<string>('')
+  const router = useRouter()
+  const { loginToast } = useCustomToasts()
 
+  const { mutate: comment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload: CommentRequest = { postId, text, replyToId }
+
+      const { data } = await axios.patch(
+        `/api/subreddit/post/comment/`,
+        payload
+      )
+      return data
+    },
+
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast()
+        }
+      }
+
+      return toast({
+        title: 'Something went wrong.',
+        description: "Comment wasn't created successfully. Please try again.",
+        variant: 'destructive',
+      })
+    },
+    onSuccess: () => {
+      router.refresh()
+      setInput('')
+    },
+  })
 
 
 

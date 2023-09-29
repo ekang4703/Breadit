2e2f6ecd3239fa9813631e152d9544c9039bb123
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-09-28 21:07:52 -0700

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,7 +3,7 @@
 import EditorJS from '@editorjs/editorjs'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { usePathname, useRouter } from 'next/navigation'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState, FC } from 'react'
 import { useForm } from 'react-hook-form'
 import TextareaAutosize from 'react-textarea-autosize'
 import { z } from 'zod'

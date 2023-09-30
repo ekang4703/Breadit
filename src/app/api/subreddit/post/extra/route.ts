a74d0d22206fe48aeea22aa510d41a8a1bd45d64
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-09-30 14:50:47 -0700

    Add files via upload

---
+++ src/app/api/subreddit/post/extra/route.ts
@@ -0,0 +1,19 @@
+import { db } from '@/lib/db'
+import { z } from 'zod'
+
+export async function GET(req: Request) {
+  try {
+    // Retrieve data from the database (for example, all posts)
+    const users = await db.user.findMany();
+
+    // Return the retrieved data as JSON in the response
+    return new Response(JSON.stringify(users), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    // Handle errors and return an appropriate response
+    return new Response('Could not fetch posts from the database.', { status: 500 });
+  }
+}

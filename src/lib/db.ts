a17972fd997d6896f9d16c5bfd400f61f934f4f6
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-01 02:33:52 -0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,3 @@
-import { PrismaClient } from '@prisma/client'
-import "server-only"
-
-declare global {
-  // eslint-disable-next-line no-var, no-unused-vars
-  var cachedPrisma: PrismaClient
-}
-
-let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
-  }
-  prisma = global.cachedPrisma
-}
+import { PrismaClient } from '@prisma/client';
 
 export const db = new PrismaClient();

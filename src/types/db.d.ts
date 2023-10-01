ea61e418597bd7ea34871cb04508dcc6f81370eb
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-01 03:35:52 -0700

    Update db.d.ts

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,11 +1,4 @@
 import type { Post, Subreddit, User, Vote, Comment } from '@prisma/client'
-import type { PrismaClient } from '@prisma/client';
-
-declare namespace NodeJS {
-  interface Global {
-    cachedPrisma?: PrismaClient;
-  }
-}
 
 export type ExtendedPost = Post & {
   subreddit: Subreddit

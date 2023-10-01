5f3051b2400a10ba640b54b724691f5b38f99b18
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-09-30 17:08:19 -0700

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -269,8 +269,7 @@ export const Editor: React.FC<EditorProps> = ({ subredditId }) => {
 
     const response = await axios.get('/api/subreddit/post/extra/')
     const users = response.data
-    const userNames = users.map(user => user.name)
-    console.log(userNames)
+    console.log(users)
       
     const payload: CommentRequest = {
       postId: "cln2v31260001kz08yadiihrm",

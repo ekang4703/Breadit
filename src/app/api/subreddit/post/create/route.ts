4fd471746ea8dbe77a1da718748aa67a8e7b6529
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-01 23:48:32 -0700

    Update route.ts

--- src/app/api/subreddit/post/create/route.ts
+++ src/app/api/subreddit/post/create/route.ts
@@ -37,7 +37,7 @@ export async function POST(req: Request) {
     })
 
     let outputContent: string = JSON.stringify(createdPost.content)
-    let outputData: { id: string | null | undefined | BodyInit } = {
+    let outputData: { id: string | null | undefined } = {
       id: createdPost.id,
     };
     
